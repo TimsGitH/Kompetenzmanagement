c9b7d0e5040f9071c685a27c27c651350de62ccf
TimRedlin <tim.redlin@tu-dortmund.de>
2025-06-30 12:55:37 +0000

    Debug-Modus wieder eingefügt

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def confirm():
 # -Titel-
 st.title("Kompetenzmanagment")
 st.selectbox(label="Bitte wählen Sie ihre Rolle aus:", options=["Mitarbeiter"], index=None, key="selected_role", placeholder="Rolle")
-# Deaktiviert: st.checkbox(label="Debug Modus", key="selected_debug_mode")
+st.checkbox(label="Debug Modus", key="selected_debug_mode")
 confirm_button = st.button(label="Bestätigen & Weiter")
 if confirm_button:
     if st.session_state.selected_role == "Mitarbeiter":
