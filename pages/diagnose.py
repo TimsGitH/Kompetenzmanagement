870bf2199e3ea8c312b9b6e1966d06a649dcdedf
Robert-En <robert.engel@tu-dortmund.de>
2025-07-10 14:45:09 +0000

    Änderungen in diagnose.py

--- pages/diagnose.py
+++ pages/diagnose.py
@@ -7,3 +7,11 @@ from functions.menu import default_menu
 # -Seitenkonfiguration-
 st.set_page_config(page_title="Diagnose", layout="wide")
 default_menu()
+
+st.title("Diagnose")
+
+# -Tabelle für Profile verknüpfen-
+data_profiles = pd.read_csv("user_management/profiles.csv", sep=';', index_col=0)
+
+set_name_active_profile = st.selectbox("Profil auswählen:", data_profiles[["Name"]])
+set_id_active_profile = data_profiles.index[data_profiles["Name"] == set_name_active_profile][0]
